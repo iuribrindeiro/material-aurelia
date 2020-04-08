39214c3e5ba7566eaa743ef81a732b2ff70a2551
Iuri <iuribrindeiro@gmail.com>
2020-04-08 18:12:55 -0300

    fix dependencies

--- src/base-material-dashboard/js/material-dashboard.js
+++ src/base-material-dashboard/js/material-dashboard.js
@@ -1,14 +1,17 @@
-(function() {
-  isWindows = navigator.platform.indexOf('Win') > -1 ? true : false;
 
-  if (isWindows) {
-    // if we are on windows OS we activate the perfectScrollbar function
-    $('.sidebar .sidebar-wrapper, .main-panel').each(e => {
-      perfectScrollbar.default(e);
-    });
-
-    $('html').addClass('perfect-scrollbar-on');
-  } else {
-    $('html').addClass('perfect-scrollbar-off');
-  }
-})();
+require(['jquery', 'perfect-scrollbar'], function ($, perfectScrollbar) {
+  (function() {
+    isWindows = navigator.platform.indexOf('Win') > -1 ? true : false;
+  
+    if (isWindows) {
+      // if we are on windows OS we activate the perfectScrollbar function
+      $('.sidebar .sidebar-wrapper, .main-panel').each(e => {
+        perfectScrollbar.default(e);
+      });
+  
+      $('html').addClass('perfect-scrollbar-on');
+    } else {
+      $('html').addClass('perfect-scrollbar-off');
+    }
+  })();
+});
